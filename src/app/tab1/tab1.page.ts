7f3915d40e3e1c824ec621a6f83d312e4f887a0f
JACaminero <juanc-r1@hotmail.com>
2021-03-04 12:30:57 -0400

    fb integration

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
+  data: Array<object> =
+  [{
+    "name": "Aline Grover",
+    "created": "November 28, 2012"
+  }, {
+    "name": "Nevada Anders",
+    "created": "January 18, 2014"
+  }, {
+    "name": "Nicholas Morissette",
+    "created": "November 11, 2014"
+  }];
 
   constructor() {}
 
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      this.addData();
+      event.target.complete();
+      if (this.data.length == 1000) {
+        event.target.disabled = true;
+      }
+    }, 1000);
+  }
+
+  addData() {
+    for (let i = 0; i < 5; i++) {
+      this.data.push(this.data[i]);
+    }
+  }
 }
